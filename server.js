51cad1fc897b25d8e79ce680d8c692c2e5778352
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-31 08:59:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,10 +23,44 @@ app.prepare().then(() => {
     console.log('Recebendo solicitação na rota /api/checkout');
     const { courses } = req.body;
 
-    // Resto do código da rota /api/checkout
-
-    // ...
-
+  
+    
+  
+    // Calcular o valor total dos cursos selecionados
+    const data = fs.readFileSync(path.join(__dirname, 'cursos.json'), 'utf8');
+    const cursos = JSON.parse(data);
+    let total = 0;
+    for (const curso of cursos) {
+      console.log(curso);
+      if (courses.map(Number).includes(curso.id)) {
+        total += parseFloat(curso.valor.replace(',', '.'));
+        console.log(total); // Adicionado aqui
+      }
+    }
+  
+    console.log('Valor total dos cursos selecionados:', total);
+  
+    const preference = {
+      items: [
+        {
+          title: 'Cursos selecionados',
+          unit_price: total,
+          quantity: 1,
+        },
+      ],
+    };
+  
+    console.log(preference);
+  
+    try {
+      console.log('Criando preferência de pagamento com o Mercado Pago');
+      const response = await mercadopago.preferences.create(preference);
+      console.log('Preferência de pagamento criada com sucesso');
+      res.status(200).json({ init_point: response.body.init_point });
+    } catch (error) {
+      console.error('Erro ao criar preferência de pagamento:', error);
+      res.status(500).json({ error: error.message });
+    }
   });
 
   server.get('/api/cursos', (req, res) => {
@@ -39,14 +73,10 @@ app.prepare().then(() => {
     }
   });
 
+
   server.all('*', (req, res) => {
     return handle(req, res);
   });
 
-  const PORT = process.env.PORT || 3000;
-
-  server.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`> Servidor pronto na porta ${PORT}`);
-  });
+  module.exports = server;
 });
